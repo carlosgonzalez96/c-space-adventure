e66811dcca0113a9c828688809f16f1cf08c2cff
carlosgonzalez96 <carlosglez817@gmail.com>
2021-05-10 21:37:54 -0700

    Added and prints introduction

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 //C space adventure
-//#include <stdio.h>
+//author: Carlos Gonzalez
+#include <stdio.h>
 int main(int argc, char *argv[]) {
+
+    printf("Welcom to the Solar System!\nThere are 9 planets to explore.\nWhat is your name?\n");
+
     return 0;
 }
