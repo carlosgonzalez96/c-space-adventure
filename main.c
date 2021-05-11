4f99155d13bf34f715aba7e7232df6c49b3e90d5
carlosgonzalez96 <carlosglez817@gmail.com>
2021-05-11 09:56:41 -0700

    User can now input a name and have it displayed in greeting

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 int main(int argc, char *argv[]) {
 
+    char name[20];
+
     printf("Welcom to the Solar System!\nThere are 9 planets to explore.\nWhat is your name?\n");
+    scanf("%19[^\n]", name);
+    printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\nLets go on an adventure!\n", name);
 
     return 0;
 }
