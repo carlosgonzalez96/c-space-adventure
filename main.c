54617708afca8275a1633dc122055ce1788805c7
carlosgonzalez96 <carlosglez817@gmail.com>
2021-05-17 09:07:16 -0700

    Handle input login for yes, no, and random

--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 //C space adventure
 //author: Carlos Gonzalez
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LIMIT 50
+
 int main(int argc, char *argv[]) {
 
-    char name[20];
+    char name[MAX_LIMIT];
+    char confirm[MAX_LIMIT];
 
-    printf("Welcom to the Solar System!\nThere are 9 planets to explore.\nWhat is your name?\n");
-    scanf("%19[^\n]", name);
+    printf("Welcome to the Solar System!\nThere are 9 planets to explore.\nWhat is your name?\n");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = 0;
     printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\nLets go on an adventure!\n", name);
+    printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
+    fgets(confirm, sizeof(confirm), stdin);
+    confirm[strcspn(confirm, "\n")] = 0;
+
+    while(strcmp(confirm, "Y") != 0 || strcmp(confirm, "N") != 0) {
+        
+        if(strcmp(confirm, "Y") == 0) {
+            printf("Barf yes\n");
+            break;
+        } else if(strcmp(confirm, "N") == 0) {
+            printf("Barf no\n");
+            break;
+        } else {
+            printf("Sorry, I didn't get that.\n");
+            printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
+            fgets(confirm, sizeof(confirm), stdin);
+            confirm[strcspn(confirm, "\n")] = 0;
+        }
+    }
 
     return 0;
 }
