5756cca16252b990f8632c0868e026dc7b7f64ea
carlosgonzalez96 <carlosglez817@gmail.com>
2021-05-17 10:51:57 -0700

    Extracted the name input  text to a function

--- main.c
+++ main.c
@@ -6,17 +6,16 @@
 #define MAX_LIMIT 50
 
 void printGreeting();
+void printName();
 
 int main(int argc, char *argv[]) {
 
-    char name[MAX_LIMIT];
     char confirm[MAX_LIMIT];
+    char planetName[MAX_LIMIT];
 
     printGreeting();
-    printf("What is your name?\n");
-    fgets(name, sizeof(name), stdin);
-    name[strcspn(name, "\n")] = 0;
-    printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\nLets go on an adventure!\n", name);
+    printName();
+    printf("Lets go on an adventure!\n");
     printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
     fgets(confirm, sizeof(confirm), stdin);
     confirm[strcspn(confirm, "\n")] = 0;
@@ -24,10 +23,13 @@ int main(int argc, char *argv[]) {
     while(strcmp(confirm, "Y") != 0 || strcmp(confirm, "N") != 0) {
         
         if(strcmp(confirm, "Y") == 0) {
-            printf("You chose: %s\n", confirm);
+            printf("Traveling to Venus...\n");
             break;
         } else if(strcmp(confirm, "N") == 0) {
-            printf("You chose: %s\n", confirm);
+            printf("Name the planet you would like to visit.\n");
+            fgets(planetName, sizeof(planetName), stdin);
+            planetName[strcspn(planetName, "\n")] = 0;
+            printf("Traveling to %s...\n", planetName);
             break;
         } else {
             printf("Sorry, I didn't get that.\n");
@@ -43,4 +45,13 @@ int main(int argc, char *argv[]) {
 void printGreeting() {
     printf("Welcome to the Solar System!\n");
     printf("There are 9 planets to explore.\n");
+}
+
+void printName() {
+    char name[MAX_LIMIT];
+
+    printf("What is your name?\n");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = 0;
+    printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
 }
