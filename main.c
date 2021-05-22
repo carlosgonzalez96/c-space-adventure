24a0ab8e2f37a133d77fb874865cfb001328b582
carlosgonzalez96 <carlosglez817@gmail.com>
2021-05-22 15:48:05 -0700

    remove spaces in program

--- main.c
+++ main.c
@@ -15,13 +15,14 @@ int main(int argc, char *argv[]) {
 
     printGreeting();
     printName();
+
     printf("Lets go on an adventure!\n");
     printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
     fgets(confirm, sizeof(confirm), stdin);
     confirm[strcspn(confirm, "\n")] = 0;
 
     while(strcmp(confirm, "Y") != 0 || strcmp(confirm, "N") != 0) {
-        
+
         if(strcmp(confirm, "Y") == 0) {
             printf("Traveling to Venus...\n");
             break;
