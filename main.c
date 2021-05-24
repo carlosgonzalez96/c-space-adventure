9de41e2eec822b829ad920840996ee53e625f7a7
carlosgonzalez96 <carlosglez817@gmail.com>
2021-05-23 19:11:55 -0700

    Extracted the while loop into a function and extracted the Y case to its own function.

--- main.c
+++ main.c
@@ -9,35 +9,14 @@ void printWelcome();
 char* responseFor();
 void printGreeting();
 void travelTo();
+void travelGuide();
+void travelToRandomPlanet();
 
 int main(int argc, char *argv[]) {
 
-    char confirm[MAX_LIMIT];
-    char planetName[MAX_LIMIT];
-
     printWelcome();
     printGreeting(responseFor("What is your name?"));
-
-    printf("Lets go on an adventure!\n");
-    printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
-    fgets(confirm, sizeof(confirm), stdin);
-    confirm[strcspn(confirm, "\n")] = 0;
-
-    while(strcmp(confirm, "Y") != 0 || strcmp(confirm, "N") != 0) {
-
-        if(strcmp(confirm, "Y") == 0) {
-            printf("Traveling to Venus...\n");
-            break;
-        } else if(strcmp(confirm, "N") == 0) {
-            travelTo(responseFor("Name the planet you would like to visit."));
-            break;
-        } else {
-            printf("Sorry, I didn't get that.\n");
-            printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
-            fgets(confirm, sizeof(confirm), stdin);
-            confirm[strcspn(confirm, "\n")] = 0;
-        }
-    }
+    travelGuide();
 
     return 0;
 }
@@ -61,4 +40,33 @@ void printGreeting(char *astring) {
 
 void travelTo(char *string) {
     printf("Traveling to %s...\n", string);
+}
+
+void travelToRandomPlanet() {
+    printf("Traveling to Venus...\n");
+}
+
+void travelGuide() {
+    char confirm[MAX_LIMIT];
+
+    printf("Lets go on an adventure!\n");
+    printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
+    fgets(confirm, sizeof(confirm), stdin);
+    confirm[strcspn(confirm, "\n")] = 0;
+
+    while(strcmp(confirm, "Y") != 0 || strcmp(confirm, "N") != 0) {
+
+        if(strcmp(confirm, "Y") == 0) {
+            travelToRandomPlanet();
+            break;
+        } else if(strcmp(confirm, "N") == 0) {
+            travelTo(responseFor("Name the planet you would like to visit."));
+            break;
+        } else {
+            printf("Sorry, I didn't get that.\n");
+            printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
+            fgets(confirm, sizeof(confirm), stdin);
+            confirm[strcspn(confirm, "\n")] = 0;
+        }
+    }
 }
