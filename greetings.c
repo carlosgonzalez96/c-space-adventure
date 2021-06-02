e826a61fad33ee4d69fc04a7bb5b751116e01662
carlosgonzalez96 <carlosglez817@gmail.com>
2021-06-01 20:33:42 -0700

    deleted unused code

--- greetings.c
+++ greetings.c
@@ -16,28 +16,3 @@ char* responseFor(char *aString) {
 void printGreeting(char *aString) {
     printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", aString);
 }
-
-// void travelGuide() {
-//     char confirm[MAX_LIMIT];
-
-//     printf("Lets go on an adventure!\n");
-//     printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
-//     fgets(confirm, sizeof(confirm), stdin);
-//     confirm[strcspn(confirm, "\n")] = 0;
-
-//     while(strcmp(confirm, "Y") != 0 || strcmp(confirm, "N") != 0) {
-
-//         if(strcmp(confirm, "Y") == 0) {
-//             travelToRandomPlanet();
-//             break;
-//         } else if(strcmp(confirm, "N") == 0) {
-//             travelTo(responseFor("Name the planet you would like to visit."));
-//             break;
-//         } else {
-//             printf("Sorry, I didn't get that.\n");
-//             printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
-//             fgets(confirm, sizeof(confirm), stdin);
-//             confirm[strcspn(confirm, "\n")] = 0;
-//         }
-//     }
-// }
--- planetTravel.c
+++ planetTravel.c
@@ -1,4 +1,5 @@
 #include "planets.h"
+#include "cJSON.h"
 
 void travelTo(char *string) {
     printf("Traveling to %s...\n", string);
