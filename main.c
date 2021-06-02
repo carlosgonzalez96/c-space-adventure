7b19fd555814002aed91e08f7e3f338af063d62e
carlosgonzalez96 <carlosglez817@gmail.com>
2021-06-01 20:26:43 -0700

    Used cJson to implement random planet

--- main.c
+++ main.c
@@ -5,23 +5,6 @@
 int main(int argc, char *argv[]) { 
 
     char* file_input = argv[1];
-    // char lines[300];
-    // char* p;
-
-    // FILE *fileData = fopen(argv[1], "r");
-
-    // if(fileData == NULL) {
-    //     fprintf(stderr, "Could not open File %s", strerror(errno));
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // while(fgets(lines, sizeof(lines), fileData) != NULL) {
-    //     p = strtok(lines, "{}[],:\"");
-    //     while(p != NULL) {
-    //         printf("%s", p);
-    //         p = strtok(NULL, "{}[],:\"");
-    //     }
-    // }
 
     printWelcome();
     printGreeting(responseFor("What is your name?"));
--- randomPlanet.c
+++ randomPlanet.c
@@ -1,12 +1,41 @@
 #include "planets.h"
+#include "cJSON.h"
 
 void travelToRandomPlanet(char* file) {
-
-    const char* planets[9] = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"};
+    char *planet_names[MAX_LIMIT];
+    char *planet_descriptions[MAX_LIMIT];
+ 
+    FILE *fp = fopen(file, "rb");
     
-    srand(time(0));
-    int nandomPlanet = rand() % 10 + 1;
+    fseek(fp, 0L, SEEK_END);
+    long size = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+    char *rJSON = (char*)calloc(size, sizeof(char));
+    fread(rJSON, sizeof(char), size, fp);
+    fclose(fp);
 
-    printf("Traveling to %s...\n", planets[nandomPlanet]);
+    const cJSON *planets = NULL;
+    const cJSON *planet = NULL;
     
+    cJSON *planets_json = cJSON_Parse(rJSON);
+
+    planets = cJSON_GetObjectItem(planets_json, "planets");
+
+    int i;
+    int planet_count = cJSON_GetArraySize(planets);
+    
+    for(i = 0; i < planet_count; i++) {
+        cJSON *planet = cJSON_GetArrayItem(planets, i);
+        cJSON *planet_name = cJSON_GetObjectItem(planet, "name");
+        cJSON *planet_desc = cJSON_GetObjectItem(planet, "description");
+        planet_names[i] = planet_name->valuestring;
+        planet_descriptions[i] = planet_desc->valuestring;
+    }
+
+    srand(time(NULL));
+    int ranandomPlanet = rand() % 9;
+
+    printf("Traveling to...%s\n", planet_names[ranandomPlanet]);
+    printf("Arrived at %s. %s\n", planet_names[ranandomPlanet], planet_descriptions[ranandomPlanet]);
+
 }
